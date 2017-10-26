a1c6ae6f8ba2431e96d7f32c11a4351904077ec6
manedks <al315557@uji.es>
2017-10-26 16:28:52 +0200

    index actualizado

--- index.php
+++ index.php
@@ -1,4 +1,42 @@
 <?php
-require_once("./principal.html");
+    //view_form.php
+    
+    /**
+     * * Descripción: Controlador principal
+     * *
+     * * Descripción extensa: Iremos añadiendo cosas complejas en PHP.
+     * *
+     * * @author  Lola <dllido@uji.es>
+     * * @copyright 2017 Lola
+     * * @license http://www.fsf.org/licensing/licenses/gpl.txt GPL 2 or later
+     * * @version 1
+     * * @link http://dllido.al.nisu.org/P0/holaMundo.php
+     * */
+
+require_once('./core/principal.php');
+require_once('./core/view.php') ;
+
+/*evitar codigo malicioso*/
+function _H($cadena){echo htmlspecialchars($cadena,ENT_QUOTES);}
+
+//variable para paso de parámetros a las plantillas.
+$data=array();
+//plantilla central por defecto del portal
+$central="./phtml/centralForm.phtml";
+
+
+if (isset($_REQUEST['module']) ){
+  $modulo=$_REQUEST["module"];
+  
+  switch ($modulo) {
+    case "user":
+      require_once('./modules/shx_shop/shop.php');
+      break;
+    default:
+      $data["error"]="Modulo no permitido";
+      $central="./phtml/error.phtml";
+  }
+}
+retornarVista($central,$data);
 
 ?>
