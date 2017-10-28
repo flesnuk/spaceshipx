630851a10e3a2690d46499b9c9137777d7e57bee
Dinu Daniel <al315567@nisu.org>
2017-10-29 00:39:59 +0200

    Landing page

--- core/view.php
+++ core/view.php
@@ -1,7 +1,6 @@
 <?php
 function retornarVista($central, $data=array()) {
   require_once("./phtml/header.phtml");
-  require_once("./phtml/menu.phtml");
   require_once($central);
   require_once("./phtml/footer.phtml");
 	
