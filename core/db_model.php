ae6b202e6f4ff11e2ea71b039a6a7578e6e6498b
flesnuk <flesnuk@gmail.com>
2017-10-30 16:02:07 +0100

    add files

---
+++ core/db_model.php
@@ -0,0 +1,65 @@
+<?php
+  
+class DBModel
+{
+    
+    private static $db_host = 'localhost';
+    private static $db_user = 'xxxx';
+    private static $db_pass = 'xxxx';
+    private static $db_name = 'xxxx';
+
+    public $query;
+    private $rows = array();
+    protected $conn;
+    public $mensaje = '';
+    
+   /* # métodos abstractos para crud(create,read,update,delete) que eredaran las clases
+    abstract protected function create();
+    abstract protected function read();
+    abstract protected function update($datos);
+    abstract protected function delete();
+    abstract protected function readAll();
+    */
+    # los siguientesbase de datos
+    private function open_connection()
+    {
+        try {
+            require_once($_SERVER['DOCUMENT_ROOT']."/../privHtml/db.php");
+            //new PDO ("mysql:host=slef::$db_host;dbname=$db_name","$db_user","$db_pass");
+            $this->conn= new PDO( "mysql:host=".self::$db_host.";dbname=".self::$db_name, self::$db_user, self::$db_pass );
+        } catch (PDOExeption $e) {
+            die($e->getMessage());
+        }
+    }
+    # Desconectar la base de datos
+    private function close_connection()
+    {
+        unset ($this->conn);
+    }
+    # Ejecutar un query simple del tipo INSERT, DELETE, UPDATE
+    function execute_single_query()
+    {
+        try {
+            $this->open_connection();
+            $this->conn->exec($this->query);
+            $this->close_connection();
+        } catch (PDOExeption $e) {
+            $this->mensaje =getMessage();
+        }
+    }
+    # Traer resultados de una consulta en un Array
+    function get_results_from_query($data_db = array())
+    {
+        $this->open_connection();
+       #echo $this->query,$data;
+        try {
+            $consult=$this->conn->prepare($this->query);
+            $consult->execute($data_db);
+            $this->rows=$consult->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOExeption $e) {
+            die($e->getMessage());
+        }
+        $this->close_connection();
+        return  $this->rows;
+    }
+}
---
+++ js/header.js
@@ -0,0 +1,41 @@
+triggers = document.getElementsByClassName('rootLink');
+triggers = [].slice.call(triggers)
+const highlight = document.createElement('span');
+
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink() {
+    highlight.style.display = 'block';
+    const linkCoords = this.getBoundingClientRect();
+    highlight.style.width = `${linkCoords.width}px`;
+    highlight.style.height = `${linkCoords.height}px`;
+    highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
+
+(function () {
+    var throttle = function (type, name, obj) {
+        obj = obj || window;
+        var running = false;
+        var func = function () {
+            if (running) { return; }
+            running = true;
+            requestAnimationFrame(function () {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+
+    /* init - you can init any event */
+    throttle("resize", "optimizedResize");
+})();
+
+// handle event
+window.addEventListener("optimizedResize", function () {
+    highlight.style.display = 'none';
+    highlight.style.transform = 'translate(0px, 0px)';
+});
